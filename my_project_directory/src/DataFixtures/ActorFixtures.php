31312f449f94109dd5cfb252f772079821bb5dfa
Axel D <axel@spliiit.com>
2023-11-22 10:29:24 +0100

    refactor : set fixtures to standart PSR2

--- my_project_directory/src/DataFixtures/ActorFixtures.php
+++ my_project_directory/src/DataFixtures/ActorFixtures.php
@@ -15,15 +15,20 @@ class ActorFixtures extends Fixture implements DependentFixtureInterface
         // Génere les données pour 10 acteurs avec un firstName et un lastName réaliste
         // Ajouter les pays
 
-        $firstNames = ['Jean', 'Pierre', 'Paul', 'Jacques', 'Marie', 'Julie', 'Julien', 'Jeanne', 'Pierre', 'Pauline'];
-        $lastNames = ['Dupont', 'Durand', 'Duchemin', 'Duchesse', 'Duc', 'Ducroc', 'Ducrocq', 'Ducroq', 'Ducroque', 'Ducroquefort'];
+        $firstNames = [
+            'Jean', 'Pierre', 'Paul', 'Jacques', 'Marie', 'Julie', 'Julien', 'Jeanne', 'Pierre', 'Pauline'
+        ];
+        $lastNames = [
+            'Dupont', 'Durand', 'Duchemin', 'Duchesse', 'Duc', 'Ducroc', 'Ducrocq', 'Ducroq', 'Ducroque', 'Ducroquefort'
+        ];
 
         foreach (range(1, 10) as $i) {
             $actor = new Actor();
             $actor->setFirstName($firstNames[rand(0, 9)]);
             $actor->setLastName($lastNames[rand(0, 9)]);
             $manager->persist($actor);
-            $this->addReference('actor_' . $i, $actor); // "expose" l'objet à l'extérieur de la classe pour les liaisons avec Movie
+            $this->addReference('actor_' . $i, $actor);
+            // "expose" l'objet à l'extérieur de la classe pour les liaisons avec Movie
         }
 
         $manager->flush();
