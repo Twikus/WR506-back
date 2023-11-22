06420013f924678fc88f503769a969955adcad0a
Axel D <axel@spliiit.com>
2023-11-22 10:46:52 +0100

    chore : add symfony/test-pack

--- my_project_directory/src/DataFixtures/MovieFixtures.php
+++ my_project_directory/src/DataFixtures/MovieFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Movie;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -14,7 +15,7 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
         foreach (range(1, 40) as $i) {
             $movie = new Movie();
             $movie->setTitle('Movie ' . $i);
-            $movie->setReleaseDate(new \DateTime());
+            $movie->setReleaseDate(new DateTime());
             $movie->setDuration(rand(60, 180));
             $movie->setDescription('Synopsis ' . $i);
             $movie->setCategory($this->getReference('category_' . rand(1, 5)));
