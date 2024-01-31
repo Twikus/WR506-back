2dd64cf1c2c389ce6bde200075253a4237f7df71
Axel D <axel@spliiit.com>
2024-01-31 10:01:54 +0100

    feat : update mediaObject

--- migrations/Version20240131080534.php
+++ migrations/Version20240131080534.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240129151617 extends AbstractMigration
+final class Version20240131080534 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Controller/MediaObjectController.php
+++ src/Controller/MediaObjectController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\MediaObject;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class MediaObjectController extends AbstractController
 {
-    public function __invoke(Request $request): MediaObject
+    public function __invoke(Request $request, MovieRepository $repo): MediaObject
     {
         $uploadedFile = $request->files->get('file');
         if (!$uploadedFile) {
@@ -18,6 +19,20 @@ class MediaObjectController extends AbstractController
 
         $mediaObject = new MediaObject();
         $mediaObject->file = $uploadedFile;
+
+        // Récupération de l'ID du film à partir de la requête
+        $movieData = json_decode($request->get('movie'), true);
+        $movieId = str_replace('/api/movies/', '', $movieData['@id']);
+
+        // Récupération de l'objet Movie correspondant à partir de la base de données
+        $movie = $repo->find($movieId);
+
+        if (!$movie) {
+            throw new BadRequestHttpException('Film non trouvé');
+        }
+
+        // Liaison de l'objet Movie à l'objet MediaObject
+        $mediaObject->setMovie($movie);
 
         return $mediaObject;
     }
