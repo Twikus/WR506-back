638278dbe8c11d9dd3cd2c122e8bd1808de5e371
Axel D <axel@spliiit.com>
2024-01-31 11:42:44 +0100

    feat : add fixtures with faker

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -10,14 +10,11 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // Génére moi 5 objets Category fictifs
-
-        foreach (range(1, 5) as $i) {
-            $category = new Category();
-            $category->setName('Category ' . $i);
+        foreach (range(1, 20) as $i) {
+            $category = (new Category())
+                ->setName('Action' . $i);
             $manager->persist($category);
             $this->addReference('category_' . $i, $category);
-            // "expose" l'objet à l'extérieur de la classe pour les liaisons avec Movie
         }
 
         $manager->flush();
